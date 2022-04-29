d96a563823e8558b8fefa2b95d41910590def2c8
Maryllian Vieira <104434426+albvieiraa@users.noreply.github.com>
2022-04-29 14:53:04 -0300

    Add files via upload

---
+++ atividade-dia-3.js
@@ -0,0 +1,30 @@
+console.log ("Atividade - Dia 3")
+
+// mapa de um produto - verifica se ele é nacional - 12%, internacional - 20% de impostos 
+
+
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false}
+
+let true = 1.2 * "valor"
+let false = 1.12 * "valor"
+
+if (camiseta == internacional) {
+    console.log ("Desconto de 20% dos impostos: " + true)
+} else{
+    console.log ("Esse produto não existe")
+}
+
+if (perfume == internacional) {
+    console.log ("Desconto de 20% dos impostos: " + true)
+} else {
+    console.log ("Esse produto não existe")
+}
+
+if (sandalia == nacional) {
+    console.log ("Desconto de 12% dos impostos: " + false)
+} else {
+    console.log ("Esse produto não existe")
+}
+
